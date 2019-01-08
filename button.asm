8fc9921907f6d23a42d02588c6e9a4dd4c4e608c
sfndude <hadoepan@gmail.com>
2019-01-08 23:18:39 +0700

    add explanations on some few lines

--- button.asm
+++ button.asm
@@ -1,15 +1,15 @@
 ;read button and turn on led
 ;atmega328p
 .device atmega328p
-	rjmp INIT
+	rjmp INIT	; first run
 INIT:
-	ldi r16,0x08
-	out 0x04,r16
-	ldi r16,0x00
-	out 0x05,r16
-	ldi r16,0x10
-	out 0x05,r16
+	ldi r16,0x08	; load immediate r16 as 0b00001000
+	out 0x04,r16	; set port b on pin 3 as output
+	ldi r16,0x00	; load immediate r16 as 0b00000000
+	out 0x05,r16	; set all pins on port b to 0 (low)
+	ldi r16,0x10	; load immediate r16 as 0b00010000
+	out 0x05,r16	; set port b on pin 4 as input pullup
 MAIN:
-	in r16,0x03
-	out 0x05,r16
-	rjmp MAIN
+	in r16,0x03	; read status port b and copy the data to r16
+	out 0x05,r16 	; write readed data to port b
+	rjmp MAIN	; relative jump to label main (looping)
