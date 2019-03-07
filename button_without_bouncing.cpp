087c5a2da1a57e656b4241518ac512d47738e3ba
mr.z3 <44674870+Sfndude@users.noreply.github.com>
2019-03-07 14:07:43 +0700

    Mini project source program mikrokontroler ELINS A

--- button_without_bouncing.cpp
+++ button_without_bouncing.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+//#include <Arduino.h> // UNCOMMENT THIS IF YOU NOT USING ARDUINO IDE
 #define INCREASE 2
 #define DECREASE 3
 #define RESET 4
@@ -8,29 +8,30 @@ int counter=0, temp=0, inc=0,
 
 void setup(){
   Serial.begin(9600);
-  PORTD|=(1<<2)|(1<<3)|(1<<4);
+  for(int x=2;x<4;x++)
+      pinMode(x,INPUT_PULLUP);
 }
 void loop(){
-  if((PIND & (1<<INCREASE))==0){
+  if(digitalRead(INCREASE)==0){
     if(inc!=1)
       counter++;
     Serial.println("Counter = "+String(counter));
     inc=1;
-  }else if((PIND & (1<<INCREASE))!=0)
+  }else if(digitalRead(INCREASE)!=0)
     inc=0;
 
-  if((PIND & (1<<DECREASE))==0){
+  if(digitalRead(DECREASE)==0){
     if(dec!=1)
       counter--;
     Serial.println("Counter = "+String(counter));
     dec=1;
-  }else if((PIND & (1<<DECREASE))!=0)
+  }else if(digitalRead(DECREASE)!=0)
     dec=0;
 
-  if((PIND & (1<<RESET))==0){
+  if(digitalRead(RESET)==0){
     counter=0;
     Serial.println("RESET!");
     Serial.println("Counter = "+String(counter));
   }
-  delay(50);
+  delay(100);
 }
