ff1328a89e131d5b65058b9c62442b5fc9f5d7ac
mr.z3 <44674870+Sfndude@users.noreply.github.com>
2019-08-10 20:05:07 +0700

    Add support for arduino
    add support access full counter for atmega328 which already bootloaded, arduino's bootloader

--- conveyor_clock_source.cpp
+++ conveyor_clock_source.cpp
@@ -1,68 +1,75 @@
 #ifndef F_CPU
   #define F_CPU 16000000UL
 #endif
-
-#define UBRR 9600
-#define BAUDRATE ((F_CPU)/(UBRR*16UL)-1)
+#define MODE 1
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
-#include <stdlib.h>
 
-volatile uint8_t ovf=1;
-volatile char msg[8];
+uint8_t m_inc=0,m_dec=0;
+uint8_t counter=0;
 
-void init_uart(void){
-  UBRR0L=(BAUDRATE);
-  UBRR0H=(BAUDRATE>>8);
-  UCSR0B|=(1<<TXEN0);
-  UCSR0C|=(1<<UCSZ00)|(1<<UCSZ01);
-}
-void uart_put(char c){
-  while(!(UCSR0A & (1<<UDRE0)));
-  UDR0=c;
-}
-void uart_puts(char* s){
-  uint8_t len=0;
-  char* temp=s;
-
-  while(*temp!=0){
-    len++;
-    temp++;
+// active low and high segment
+// common anode and cathode
+const uint8_t segment[2][10]={
+  {0x3f,0x06,0x5b,0x4f,0x66,
+  0x6d,0x7d,0x07,0x7f,0x6f},
+  {0xC0,0xF9,0xA4,0xB0,0x99,
+  0x92,0x82,0xF8,0x00,0x10}
+};
+void write_segment(uint8_t index){ PORTD=((segment[MODE][index])<<1); }
+void switch_segment(uint8_t index){
+  if(index==0){
+    PORTB|=(1<<1);
+    PORTB&= ~(1<<0);
+  }else{
+    PORTB|=(1<<0);
+    PORTB&= ~(1<<1);
   }
-  for(int x=0;x<len;x++)
-    uart_put(s[x]);
 }
-
-void init_timer1(void){
-  TCCR1B|=(1<<CS10)|(1<<CS11)|(1<<CS12);
-  TCNT1=65534;
-  TIMSK1|=(1<<TOIE1);
-  sei();
-}
-
-ISR(TIMER1_OVF_vect){
-  TCNT1=65534;
-
-  if(ovf>10) uart_puts("JOB ENDED!\n");
-  else{
-    itoa(ovf,msg,10);
-    PORTB|=(1<<5);
-    uart_puts("COUNTER = ");
-    uart_puts(msg);
-    uart_put('\n');
-    ovf++;
-    PORTB&= ~(1<<5);
+void write_number(uint8_t n){
+  if(n<10){
+    switch_segment(0);
+    write_segment(0);
+    _delay_ms(5);
+    switch_segment(1);
+    write_segment(n);
+    _delay_ms(5);
+  }else{
+    switch_segment(0);
+    write_segment((n/10));
+    _delay_ms(5);
+    switch_segment(1);
+    write_segment(n-(int(n/10)*10));
+    _delay_ms(5);
   }
 }
 
 int main(void){
-  DDRB|=(1<<5)|(1<<4);
-  PORTD|=(1<<5)|(1<<4);
-  init_uart();
-  init_timer1();
-  uart_puts("READY TO RUN COMMAND!\n");
+  UCSR0B&= ~(1<<TXEN0);
+  for(uint8_t x=1;x<8;x++)
+    DDRD|=(1<<x);
+  DDRB|=(1<<0)|(1<<1);
+  PORTB|=(1<<2)|(1<<3);
 
-  while(1);
+  while(1){
+    if((PINB & (1<<2))==0){
+      if(m_inc!=0){
+        if(counter>=99) counter=99;
+        else counter++;
+      }
+      m_inc=0;
+    }else if((PINB & (1<<2))!=0)
+      m_inc=1;
+
+    if((PINB & (1<<3))==0){
+      if(m_dec!=0){
+        if(counter<=0) counter=0;
+        else counter--;
+      }
+      m_dec=0;
+    }else if((PINB & (1<<3))!=0)
+      m_dec=1;
+    write_number(counter);
+  }
 }
